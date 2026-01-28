dee694ecc4375392bd9211139b515c9091213984
zeeshan khan <95221847+Zee31-16-05@users.noreply.github.com>
2026-01-28 08:52:33 +0000

    3rd question

--- file19-01-26.js
+++ file19-01-26.js
@@ -11,14 +11,34 @@
 // Input: ['node', 'express', 'node', 'js']
 // Output: { node: 2, express: 1, js: 1 }
 
-let arr = ['node', 'express', 'node', 'js'];
-const output = arr.reduce(((acc,curr)=>{
-    if(acc[curr]){
-        acc[curr]++;
+// let arr = ['node', 'express', 'node', 'js'];
+// const output = arr.reduce(((acc,curr)=>{
+//     if(acc[curr]){
+//         acc[curr]++;
+//     }
+//     else{
+//        acc[curr]=1 
+//     }
+//     return acc;     //always return the accumulator while using reduce my friend.
+// }),{})
+// console.log(output);
+
+//Q.3 Given an array of objects, remove duplicates based on a specific key (id).
+let Input = [
+  { id: 1, name: 'A' },
+  { id: 2, name: 'B' },
+  { id: 1, name: 'C' }
+]
+// Output: [
+//   { id: 1, name: 'A' },
+//   { id: 2, name: 'B' }
+// ]
+
+let arr = Input.reduce(((acc,curr)=>{
+    if(!acc.some(obj=> obj.id === curr.id)){
+         acc.push(curr)
     }
-    else{
-       acc[curr]=1 
-    }
-    return acc;     //always return the accumulator while using reduce my friend.
-}),{})
-console.log(output);
+return acc;
+}),[])
+
+console.log(arr);
