c6681f931299f61731dca939df314327cf68196f
zeeshan khan <95221847+Zee31-16-05@users.noreply.github.com>
2026-01-28 03:31:14 +0000

    2nd question

--- file19-01-26.js
+++ file19-01-26.js
@@ -1,9 +1,24 @@
-let value = "Nodejs is awesome!";
-console.log(value.split(" "));
-let result = [];
+// let value = "Nodejs is awesome!";
+// console.log(value.split(" "));
+// let result = [];
 
-for(let i = value.split(" ").length; i > 0; i--){
-    result.push(value.split(" ")[i - 1]); 
-}
+// for(let i = value.split(" ").length; i > 0; i--){
+//     result.push(value.split(" ")[i - 1]); 
+// }
+// console.log(result);
 
-console.log(result);
+//Q.2
+// Input: ['node', 'express', 'node', 'js']
+// Output: { node: 2, express: 1, js: 1 }
+
+let arr = ['node', 'express', 'node', 'js'];
+const output = arr.reduce(((acc,curr)=>{
+    if(acc[curr]){
+        acc[curr]++;
+    }
+    else{
+       acc[curr]=1 
+    }
+    return acc;     //always return the accumulator while using reduce my friend.
+}),{})
+console.log(output);
