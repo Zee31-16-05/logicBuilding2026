ab1ea1071f6da06e6573d444db05155565987d36
zeeshan khan <95221847+Zee31-16-05@users.noreply.github.com>
2026-02-21 16:46:10 +0000

    new code

--- 6thFeb.js
+++ 6thFeb.js
@@ -261,3 +261,61 @@ const result21 = users.reduce((acc,user)=>{
 console.log(result21)
 
 
+
+
+// Deep Compare Two Objects
+const obj1 = { a: 1, b: { c: 2 } };
+const obj2 = { a: 1, b: { c: 3 } };
+// Output: true
+
+function deepCompare(obj1, obj2) {
+  // if (obj1 === obj2) return true;
+  let count = 0
+  let key
+for(key in obj1){
+  if(obj1[key] === obj2[key] && typeof obj1[key] !== 'object' && typeof obj2[key] !== 'object'){
+    count++;
+  }
+  else{
+    for(key in obj1){
+      if(obj1[key][key] !== obj2[key][key]){
+     if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+        if (!deepCompare(obj1[key], obj2[key])) {
+          return false;
+        }
+      } else {
+        return false;
+      }
+      function deepCompare(obj1, obj2) {
+        if (obj1 === obj2) return true;
+        
+        if (typeof obj1 !== 'object' || typeof obj2 !== 'object' || obj1 === null || obj2 === null) {
+          return false;
+        }
+        
+        const keys1 = Object.keys(obj1);
+        const keys2 = Object.keys(obj2);
+        
+        if (keys1.length !== keys2.length) return false;
+        
+        for (let key of keys1) {
+          if (!keys2.includes(key)) return false;
+          
+          if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+            if (!deepCompare(obj1[key], obj2[key])) return false;
+          } else if (obj1[key] !== obj2[key]) {
+            return false;
+          }
+        }
+        
+        return true;
+      }
+      
+    }
+  }
+  // return true;
+}
+}
+}
+const result112 = deepCompare(obj1, obj2)
+console.log(result112);
